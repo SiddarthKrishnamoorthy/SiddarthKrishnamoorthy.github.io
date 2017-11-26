0e454c3172ecb0ac84cd590c3ffa4b819b807701
K Siddarth <siddarrth.k@gmail.com>
2017-11-26 15:51:11 +0530

    Added deployment script v0

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+setup(){
+    git config --global user.email travis@travis-ci.org
+    git config --global user.name "Travis CI"
+}
+
+commit_files(){
+    git checkout -b deployment
+    mv pages/* ./
+    git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+push_master(){
+    git push -f -q https://$GITHUB_TOKEN@github.com/SiddarthKrishnamoorthy/SiddarthKrishnamoorthy.github.io master &2>/dev/null
+}
+
+setup
+commit_files
+push_master
